7237350e0850381de1b4940f7ac9cbaf0dd1b408
Daniel Solawa <danielsolawa@gmail.com>
2018-02-08 18:33:17 +0100

    added update inventory

--- store-client/src/main/resources/static/js/controllers/inventory-controller.js
+++ store-client/src/main/resources/static/js/controllers/inventory-controller.js
@@ -1,18 +1,45 @@
 application.controller('inventoryController',['$rootScope','inventoryService', function($rootScope, inventoryService){
     var self = this;
     self.inventory = [];
-    self.filteredInventory = [];
 
+
+    self.getTotalPrice = function(){
+        for(var i = 0; i < self.filteredInventory.length; i++){
+            self.totalPrice += (self.filteredInventory[i].price * self.filteredInventory[i].amount);
+        }
+    }
 
     self.fetchInventory = function(){
+        self.filteredInventory = [];
+        self.totalPrice = 0;
         inventoryService.get({id: $rootScope.user.id}, function(response){
-
+            $rootScope.inventory = response;
             self.inventory = response.products;
             prepareInventory();
 
         }, function(error){
             console.log("error");
         });
+    }
+
+
+    self.updateInventory = function(){
+
+      var updatedInventory = {userId: $rootScope.user.id ,products:[]};
+      for(var i = 0; i < self.filteredInventory.length; i++){
+          for(var j = 0; j < self.filteredInventory[i].amount; j++){
+              updatedInventory.products.push({id: self.filteredInventory[i].id,
+              name: self.filteredInventory[i].name, price: self.filteredInventory[i].price});
+          }
+      }
+
+
+      inventoryService.update({id: $rootScope.user.id}, updatedInventory, function(response){
+
+          self.fetchInventory();
+      }, function(error){
+          console.log(error);
+      });
     }
 
     var prepareInventory = function(){
@@ -29,6 +56,8 @@ application.controller('inventoryController',['$rootScope','inventoryService', f
 
         }
 
+        self.getTotalPrice();
+
     }
 
 
@@ -37,7 +66,10 @@ application.controller('inventoryController',['$rootScope','inventoryService', f
             id: product.id, name: product.name, price: product.price,
             description: product.description, amount: 1
         });
+
     }
+
+
 
 
 
