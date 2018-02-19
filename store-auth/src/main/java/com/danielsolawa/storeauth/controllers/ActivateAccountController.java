ec4edd70c8fbd371fdf8bead436165d655b0b799
Daniel Solawa <danielsolawa@gmail.com>
2018-02-19 12:16:50 +0100

    created ActivateAccountController + unit tests

---
+++ store-auth/src/main/java/com/danielsolawa/storeauth/controllers/ActivateAccountController.java
@@ -0,0 +1,26 @@
+package com.danielsolawa.storeauth.controllers;
+
+
+import com.danielsolawa.storeauth.services.ActivateAccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(ActivateAccountController.BASE_URL)
+public class ActivateAccountController {
+
+    public static final String BASE_URL =  "/api/v1/users";
+
+    private final ActivateAccountService activateAccountService;
+
+    public ActivateAccountController(ActivateAccountService activateAccountService) {
+        this.activateAccountService = activateAccountService;
+    }
+
+    @GetMapping("/{userId}/activate/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    public void activateAccount(@PathVariable Long userId, @PathVariable String token){
+        activateAccountService.activateAccount(userId, token);
+
+    }
+}
---
+++ store-auth/src/test/java/com/danielsolawa/storeauth/controllers/ActivateAccountControllerTest.java
@@ -0,0 +1,46 @@
+package com.danielsolawa.storeauth.controllers;
+
+import com.danielsolawa.storeauth.services.ActivateAccountService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ActivateAccountControllerTest {
+
+    MockMvc mockMvc;
+
+    @Mock
+    ActivateAccountService activateAccountService;
+
+    ActivateAccountController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        controller = new ActivateAccountController(activateAccountService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void activateAccount() throws Exception {
+        mockMvc.perform(get
+                (ActivateAccountController.BASE_URL + "/1/activate/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        then(activateAccountService).should().activateAccount(anyLong(), anyString());
+
+    }
+}
