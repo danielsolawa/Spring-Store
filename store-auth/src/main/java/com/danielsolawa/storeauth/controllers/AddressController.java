eeb74c22713a2ae89f8ff65e725477635c1e17ff
Daniel Solawa <danielsolawa@gmail.com>
2018-02-22 12:48:28 +0100

    implemented AddressController + unit tests

---
+++ store-auth/src/main/java/com/danielsolawa/storeauth/controllers/AddressController.java
@@ -0,0 +1,31 @@
+package com.danielsolawa.storeauth.controllers;
+
+import com.danielsolawa.storeauth.dtos.AddressDto;
+import com.danielsolawa.storeauth.services.AddressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(AddressController.BASE_URL)
+public class AddressController {
+
+    public static final String BASE_URL = "/api/v1/users";
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PostMapping("/{userId}/address")
+    @ResponseStatus(HttpStatus.OK)
+    public AddressDto createNewAddress(@PathVariable Long userId, @RequestBody AddressDto addressDto){
+        return addressService.createNewAddress(userId, addressDto);
+    }
+
+    @PutMapping("/{userId}/address")
+    @ResponseStatus(HttpStatus.OK)
+    public AddressDto updateAddress(@PathVariable Long userId, @RequestBody AddressDto addressDto){
+        return addressService.updateAddress(userId, addressDto);
+    }
+}
---
+++ store-auth/src/test/java/com/danielsolawa/storeauth/controllers/AddressControllerTest.java
@@ -0,0 +1,81 @@
+package com.danielsolawa.storeauth.controllers;
+
+import com.danielsolawa.storeauth.dtos.AddressDto;
+import com.danielsolawa.storeauth.services.AddressService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AddressControllerTest extends AbstractControllerTest{
+
+    MockMvc mockMvc;
+
+    AddressController addressController;
+
+    @Mock
+    AddressService addressService;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        addressController = new AddressController(addressService);
+        mockMvc = MockMvcBuilders.standaloneSetup(addressController).build();
+
+    }
+
+    @Test
+    public void createNewAddress() throws Exception {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(1L);
+
+        given(addressService.createNewAddress(anyLong(), Matchers.any())).willReturn(addressDto);
+
+        mockMvc.perform(post(AddressController.BASE_URL + "/1/address")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(asJson(addressDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)));
+
+
+        then(addressService).should().createNewAddress(anyLong(), Matchers.any());
+
+
+    }
+
+    @Test
+    public void updateAddress() throws Exception {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setId(1L);
+
+        given(addressService.updateAddress(anyLong(), Matchers.any())).willReturn(addressDto);
+
+        mockMvc.perform(put(AddressController.BASE_URL + "/1/address")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(asJson(addressDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)));
+
+
+        then(addressService).should().updateAddress(anyLong(), Matchers.any());
+
+
+    }
+}
